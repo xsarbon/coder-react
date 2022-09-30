548acc1a1ff6c916f20058aef689637d58e9e08d
Nicolas Santucho <nicolaselias1998@gmail.com>
2022-09-30 20:28:14 -0300

    correcciones

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,4 @@
 
-
 const Cart = () => {
     return (
         <div>Cart</div>
--- src/components/CartWidget/Cart.jsx
+++ src/components/CartWidget/Cart.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 import { NavLink } from "react-router-dom"
+import CartContextProvider from "../../context/CartContext"
 
 function Cart() {
     return (
         <NavLink to="/Cart">
             <ShoppingCartRoundedIcon className="cart" color="primary" fontSize="large" />
+
         </NavLink>
     );
 }
